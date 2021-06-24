74524d4667272e1dc18425f60174608adfed3c9d
phuochungdn1999 <phuochungdn1999@gmail.com>
2021-06-24 15:01:38 +0700

    send transaction

--- app.js
+++ app.js
@@ -3,28 +3,30 @@ const Tx = require('ethereumjs-tx').Transaction;
 const web3 = new Web3('https://rinkeby.infura.io/v3/fffda8246d9241f2aa056b563090838d');
 
 async function send(privateKey,receiver,amount){
-    const sender = web3.eth.accounts.privateKeyToAccount(privateKey);
-    if(!web3.utils.isAddress(sender.address)){
-        console.log('Invalid private key');
-        return;
-    }
-    if(!web3.utils.isAddress(receiver)){
-        console.log('Invalid receiver address');
-        return;
-    }
+    
     try{
+        try {
+            var sender  = web3.eth.accounts.privateKeyToAccount(privateKey);
+        } catch (error) {
+            throw new Error('Invalid Private key')
+        }
+        if(!web3.utils.isAddress(receiver)){
+            throw new Error('Invalid receiver address')
+        }
         const privateKeyToHex = Buffer.from(privateKey,'hex');
-        const nonce = await web3.eth.getTransactionCount('123');
+        const nonce = await web3.eth.getTransactionCount(sender.address);
         console.log('address',sender.address)
+
         //build the transaction
         const txObject = {
             nonce: nonce,
             to: receiver,
             value: web3.utils.toHex(web3.utils.toWei(amount,'ether')), 
-            gasLimit: web3.utils.toHex(21000),
-            gasPrice: web3.utils.toHex(web3.utils.toWei('10','gwei'))
         }
-        
+
+        txObject.gasLimit =  300000
+        txObject.gasPrice = web3.utils.toHex(await web3.eth.getGasPrice())
+        console.log(txObject)
         //sign the transaction
         const tx = new Tx(txObject, {chain:'rinkeby'})
         tx.sign(privateKeyToHex)
@@ -39,10 +41,11 @@ async function send(privateKey,receiver,amount){
         
         
     }catch(err){
-        console.log('123',err)
+        console.log('error: ',err)
     }
     
 
 }
 
-send('9c36bd51fd273f4b4843a76a6c83ab0931c7f5876806297e0b8112b29dd6c0ef','0xc860c73f034D9182fafF474ACA6B61bdC4cF1997','0.00001');
+send('9c36bd51fd273f4b4843a76a6c83ab0931c7f5876806297e0b8112b29dd6c0ef','0xc860c73f034D9182fafF474ACA6B61bdC4cF1997','0.00000001');
+
