515fb1d76abf35a4fb4285e5bdce25c82a316281
phuochungdn1999 <phuochungdn1999@gmail.com>
2021-06-24 11:01:39 +0700

    send transaction

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
-const Tx = require('ethereumjs-tx').Transaction;
 const Web3 = require('web3');
-const web3 = new Web3('https://ropsten.infura.io/v3/fffda8246d9241f2aa056b563090838d');
+const Tx = require('ethereumjs-tx').Transaction;
+const web3 = new Web3('https://rinkeby.infura.io/v3/fffda8246d9241f2aa056b563090838d');
 
-const account1 = '0x6456be06d125C0B7F661E6E09E695AF4d59D58D1';
-const account2 = '0xc860c73f034D9182fafF474ACA6B61bdC4cF1997';
-
-const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1,'hex');
-const privateKey2 = Buffer.from(process.env.PRIVATE_KEY_2,'hex');
-
-
-web3.eth.getTransactionCount(account1,(err,txCount)=>{
-    //build the transaction
-    const txObject = {
-        nonce:web3.utils.toHex(txCount),
-        to:account2,
-        value: web3.utils.toHex(web3.utils.toWei('0.5','ether')), 
-        gasLimit: web3.utils.toHex(21000),
-        gasPrice: web3.utils.toHex(web3.utils.toWei('10','gwei'))
+async function send(privateKey,receiver,amount){
+    const sender = web3.eth.accounts.privateKeyToAccount(privateKey);
+    if(!web3.utils.isAddress(sender.address)){
+        console.log('Invalid private key');
+        return;
     }
+    if(!web3.utils.isAddress(receiver)){
+        console.log('Invalid receiver address');
+        return;
+    }
+    try{
+        const privateKeyToHex = Buffer.from(privateKey,'hex');
+        const nonce = await web3.eth.getTransactionCount('123');
+        console.log('address',sender.address)
+        //build the transaction
+        const txObject = {
+            nonce: nonce,
+            to: receiver,
+            value: web3.utils.toHex(web3.utils.toWei(amount,'ether')), 
+            gasLimit: web3.utils.toHex(21000),
+            gasPrice: web3.utils.toHex(web3.utils.toWei('10','gwei'))
+        }
+        
+        //sign the transaction
+        const tx = new Tx(txObject, {chain:'rinkeby'})
+        tx.sign(privateKeyToHex)
+    
+        const serializedTransaction = tx.serialize()
+        const raw = '0x'+serializedTransaction.toString('hex')
+        console.log(raw)
+        // //broadcast the transaction
+        const transaction = await web3.eth.sendSignedTransaction(raw)
+        
+        console.log(transaction)
+        
+        
+    }catch(err){
+        console.log('123',err)
+    }
+    
 
-    // //sign the transaction
-    const tx = new Tx(txObject, {chain:'ropsten', hardfork: 'petersburg'})
-    tx.sign(privateKey1)
+}
 
-    const serializedTransaction = tx.serialize()
-    const raw = '0x'+serializedTransaction.toString('hex')
-    console.log(raw)
-    // //broadcast the transaction
-    web3.eth.sendSignedTransaction(raw,(err,txHash)=>{
-        console.log(err)
-        console.log('txHash: ',txHash)
-    })
-})
-
-
-
+send('9c36bd51fd273f4b4843a76a6c83ab0931c7f5876806297e0b8112b29dd6c0ef','0xc860c73f034D9182fafF474ACA6B61bdC4cF1997','0.00001');
