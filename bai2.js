f640db40be88e7fc5975c6704c9a6c12fd8e9b96
phuochungdn1999 <phuochungdn1999@gmail.com>
2021-07-03 11:12:46 +0700

    test

--- bai2.js
+++ bai2.js
@@ -4,32 +4,35 @@ const newZigZag = (input,n)=>{
     let down = 0;
     let newString = '';
     let check = true; 
-    for(let i = 0; i <n; i++){
-        newString += input[i];
-        if(i=== 0|| i === n-1){
-            for(let j = i;j<input.length- jump;j+=jump){
-                newString += input[j+jump];
-            }
-        }else{
-            up -= 2;
-            down += 2;
-
-            for(let j=i;j<input.length-2;){
-                if((j+up>=input.length&&check)||(j+down>=input.length&&!check))
-                break;
-                if(check){
-                    newString += input[j+up];
-                    check = !check;
-                    j+= up;
-                }else{
-                    newString += input[j+down];
-                    j+= down
-                    check = !check;
+    if(n>1){
+        for(let i = 0; i <n; i++){
+            newString += input[i];
+            if(i=== 0|| i === n-1){
+                for(let j = i;j<input.length- jump;j+=jump){
+                    newString += input[j+jump];
                 }
-                
+            }else{
+                up -= 2;
+                down += 2;
+
+                for(let j=i;j<input.length-2;){
+                    if((j+up>=input.length&&check)||(j+down>=input.length&&!check))
+                    break;
+                    if(check){
+                        newString += input[j+up];
+                        check = !check;
+                        j+= up;
+                    }else{
+                        newString += input[j+down];
+                        j+= down
+                        check = !check;
+                    }
+                    
+                }
             }
         }
-    }
+    }else newString = input;
+    
     return newString;
 }
 console.log(newZigZag('BEADVANCEDPARTNER',3))
