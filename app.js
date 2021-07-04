494d2ab6e893517531fcecfe70473eb0276b7e5e
phuochungdn1999 <phuochungdn1999@gmail.com>
2021-07-04 21:53:42 +0700

    send transaction

--- app.js
+++ app.js
@@ -2,35 +2,49 @@ const Web3 = require('web3');
 const Tx = require('ethereumjs-tx').Transaction;
 const web3 = new Web3('https://rinkeby.infura.io/v3/fffda8246d9241f2aa056b563090838d');
 
-const abi = require('./abis/ERC20.json')
-console.log(abi)
+async function send(privateKey,receiver,amount){
+    
+    try{
+        try {
+            var sender  = web3.eth.accounts.privateKeyToAccount(privateKey);
+        } catch (error) {
+            throw new Error('Invalid Private key')
+        }
+        if(!web3.utils.isAddress(receiver)){
+            throw new Error('Invalid receiver address')
+        }
+        const privateKeyToHex = Buffer.from(privateKey,'hex');
+        const nonce = await web3.eth.getTransactionCount(sender.address);
+        console.log('address',sender.address)
 
-async function loadContract() {
-    return await new web3.eth.Contract(abi, '0xFb911c5c75d951e04054fd84c021568c0f11985D');
+        //build the transaction
+        const txObject = {
+            nonce: nonce,
+            to: receiver,
+            value: web3.utils.toHex(web3.utils.toWei(amount,'ether')), 
+        }
+
+        txObject.gasLimit = await web3.eth.estimateGas(txObject)
+        txObject.gasPrice = web3.utils.toHex(await web3.eth.getGasPrice())
+        console.log(txObject)
+        //sign the transaction
+        const tx = new Tx(txObject, {chain:'rinkeby'})
+        tx.sign(privateKeyToHex)
+    
+        const serializedTransaction = tx.serialize()
+        const raw = '0x'+serializedTransaction.toString('hex')
+        console.log(raw)
+        // //broadcast the transaction
+        const transaction = await web3.eth.sendSignedTransaction(raw)
+        
+        console.log(transaction)
+        
+        
+    }catch(err){
+        console.log('error: ',err)
+    }
+    
+
 }
 
-const load = async ()=>{
-    const ERC = await loadContract();
-    // console.log( await ERC.methods.totalSupply().call())
-    return ERC;
-}
-
-const getDetail = async (ERC)=>{
-    const name = await ERC.methods.name().call()
-    const symbol = await ERC.methods.symbol().call()
-    const totalSupply = await ERC.methods.totalSupply().call()
-    return  {name,symbol,totalSupply}
-}
-const getBalance = async (ERC)=>{
-    const balance = await ERC.methods.balanceOf('0xc860c73f034D9182fafF474ACA6B61bdC4cF1997').call()
-    return  balance
-}
-
-
-const run = async ()=>{
-    const ERC = await load()
-    console.log(await getDetail(ERC))
-    console.log(await getBalance(ERC))
-}
-run()
-
+send('9c36bd51fd273f4b4843a76a6c83ab0931c7f5876806297e0b8112b29dd6c0ef','0xc860c73f034D9182fafF474ACA6B61bdC4cF1997','0.00000001');
