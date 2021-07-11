d8b8aa41fffc953791d9dc9149560b05e9b8bb1c
phuochungdn1999 <phuochungdn1999@gmail.com>
2021-07-11 12:46:34 +0700

    finsh ERC20

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ const abi = require('./abis/ERC20.json')
 
 const {account1,account2,contractAddress,privateKey1,privateKey2} = require('./contract');
 
-async function loadContract() {
-    return await new web3.eth.Contract(abi, contractAddress);
+
+factoryAddress = "0x5aEF7f124CC539F623f24f566223e92DA0378b9E"
+
+bytecode = "0x6080604052600560005534801561001557600080fd5b5060b3806100246000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806306540f7e14602d575b600080fd5b60336047565b604051603e9190605a565b60405180910390f35b60005481565b6054816073565b82525050565b6000602082019050606d6000830184604d565b92915050565b600081905091905056fea264697066735822122007c98ae01b83d513026c944b73f15dd1f1fafe9e2f8231a21b4a5bcd92e646a464736f6c63430008010033"
+
+salt = 1;
+
+console.log("0x" + web3.utils.sha3('0xff' + factoryAddress.slice(2) + web3.eth.abi.encodeParameter('uint256',salt).slice(2).toString() + web3.utils.sha3(bytecode).slice(2).toString()).slice(-40));
+
+console.log(web3.eth.abi.encodeParameter('address', "0x6456be06d125C0B7F661E6E09E695AF4d59D58D1"))
+
+console.log(web3.utils.keccak256("PROXIABLE"))
+
+function loadContract() {
+    return new web3.eth.Contract(abi, contractAddress);
 }
+
+const temp = "0x6080604052600560005534801561001557600080fd5b5060b3806100246000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806306540f7e14602d575b600080fd5b60336047565b604051603e9190605a565b60405180910390f35b60005481565b6054816073565b82525050565b6000602082019050606d6000830184604d565b92915050565b600081905091905056fea264697066735822122007c98ae01b83d513026c944b73f15dd1f1fafe9e2f8231a21b4a5bcd92e646a464736f6c634300080100330000000000000000000000006456be06d125c0b7f661e6e09e695af4d59d58d1"
 
 const load = async ()=>{
     const ERC = await loadContract();
@@ -23,12 +38,13 @@ const getDetail = async (ERC)=>{
     return  {name,symbol,totalSupply}
 }
 const getBalance = async (ERC)=>{
-    const balance = await ERC.methods.balanceOf(account2).call()
+    const balance = await ERC.methods.balanceOf(account1).call()
     return  {balance}
 }
 
 const transfer = async (from,to,amount,ERC)=>{
     const data = ERC.methods.transfer(to,amount).encodeABI()
+    console.log({data})
     const nonce = await web3.eth.getTransactionCount(from);
     const txObject = {
         nonce: nonce,
@@ -52,7 +68,15 @@ const transfer = async (from,to,amount,ERC)=>{
 }
 
 const getLog = async (ERC,name)=>{
-    ERC.getPastEvents(name, { fromBlock: 0 }).then((events) => console.log({events}));
+    // ERC.getPastEvents(name, { fromBlock: 0 }).then((events) => console.log({events}));
+    const events = await ERC.getPastEvents(name, {
+        filter: {to:'0x0448858fCAF2c8E3F1aAd5426D1494031eA4532c'}, // Using an array means OR: e.g. 20 or 23
+        fromBlock: 0,
+        toBlock: 'latest'})
+    events.forEach(event => {
+        console.log({event: event.returnValues})
+    });
+    
 
 }
 
@@ -97,12 +121,12 @@ const signTransaction = async (txObject,privateKey)=>{
 const run = async ()=>{
     const ERC = await load()
     console.log(await getDetail(ERC))
-    transfer(account1,account2,167,ERC)
+    transfer(account1,contractAddress,456,ERC)
     // console.log(await getBalance(ERC))
     // console.log(account1)
     // approve(account1,account2,111111,ERC);
     // getLog(ERC,'Transfer')
     // getLog(ERC,'Approval')
 }
-run()
+// run()
 
